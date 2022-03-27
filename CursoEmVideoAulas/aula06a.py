c2a4570b68729c31af4f854cc933b15d2d03f77a
João Paulo de Albuquerque Santos <joao.2019paulo.2020@gmail.com>
2022-03-27 18:24:32 -0300

    criando repositório

---
+++ CursoEmVideoAulas/aula06a.py
@@ -0,0 +1,6 @@
+# Para adicionar um inteiro em uma variável, precisa usar um cast
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+# Usando o 'f' antes das aspas simples, é possível formatar 
+print(f'A soma entre {n1} e {n2} vale {s}')
---
+++ CursoEmVideoAulas/aula06b.py
@@ -0,0 +1,18 @@
+# Convertendo para string.
+s = str(input('Digite um texto: '))
+print(f'Você digitou - {s}')
+
+# Convertendo para int.
+n = int(input('Digite um número inteiro: '))
+print(f'Você digitou - {n}')
+
+# Convertendo para float.
+n1 = float(input("Digite um número float: "))
+print(f'Você digitou - {n1}')
+
+# Convertendo para bool.
+# Neste caso, se for digitado algo,
+# o valor da variável será True,
+# caso contrário, será False.
+v = bool(input('Digite um algo: '))
+print(f'Você digitou? {v}')
